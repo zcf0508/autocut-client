0501926a0197ab63ec7804878818cf5e811f2b90
zcf0508 <zcf0508@live.com>
2023-02-14 17:39:20 +0800

    fix: Update `ffmpeg` and `autocut` version checks

--- src/main/autocut/check.ts
+++ src/main/autocut/check.ts
@@ -4,21 +4,32 @@ import { savePath } from "~~/utils"
 export function ffmpegCheck() {
   return new Promise<boolean>((resolve, reject) => {
     const ffmpeg = exec("ffmpeg -version")
+    let success = false
 
     ffmpeg.stdout.on("data", (res) => {
+      // console.log(`stdout: ${res}`)
       if(res.indexOf("ffmpeg version") >= 0){
+        success = true
         resolve(true)
         return
       }
     })
 
     ffmpeg.stderr.on("data", (err) => {
-      resolve(false)
-      return
+      // console.log(`stderr: ${err}`)
+      if(err.indexOf("ffmpeg version") >= 0){
+        success = true
+        resolve(true)
+        return
+      }
     })
 
     ffmpeg.on("close", (code, signal) => {
       console.log(`ffmpeg exit. ${code}: ${signal}`)
+      if (!success) {
+        resolve(false)
+      }
+      return
     })
   })
 }
@@ -31,20 +42,31 @@ export function autocutCheck(excutePath:string){
   return new Promise<boolean>((resolve, reject) => {
     const commad = `"${savePath(excutePath)}" -h`
     const autocut = exec(commad)
+    let success = false
     autocut.stdout.on("data", (res) => {
+      // console.log(`stdout: ${res}`)
       if(res.indexOf("usage: autocut") >= 0){
         resolve(true)
+        success = true
         return
       }
     })
 
     autocut.stderr.on("data", (err) => {
-      resolve(false)
-      return
+      // console.log(`stderr: ${err}`)
+      if(err.indexOf("usage: autocut") >= 0){
+        resolve(true)
+        success = true
+        return
+      }
     })
 
     autocut.on("close", (code, signal) => {
       console.log(`AutoCut exit. ${code}: ${signal}`)
+      if (!success) {
+        resolve(false)
+      }
+      return
     })
   })
 }
