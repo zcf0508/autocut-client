7220e4d606999b6848fc9c03eba826d04e588e16
zcf0508 <zcf0508@live.com>
2023-09-20 16:08:29 +0800

    fix: resolve path

--- script/post.js
+++ script/post.js
@@ -1,42 +1,70 @@
 const shelljs = require("shelljs")
 const fs = require("node:fs")
+const path = require("node:path")
 
-
-if(!fs.existsSync("./lib/whisper.cpp/Makefile")) {
-  shelljs.exec("git clone https://github.com/ggerganov/whisper.cpp.git lib/whisper.cpp -b v1.4.0 --depth=1")
-  fs.cpSync("./script/whisper-addon.cpp", "./lib/whisper.cpp/examples/addon.node/addon.cpp")
+function info(msg) {
+  shelljs.echo("\x1b[32m" + msg + "\x1b[0m")
 }
 
-if(!fs.existsSync("./lib/whisper.cpp/build/Release/whisper-addon.node")) {
+if(!fs.existsSync(path.resolve(__dirname, "../lib/whisper.cpp/Makefile"))) {
+  info("Clone whisper.cpp")
+  shelljs.exec("git clone https://github.com/ggerganov/whisper.cpp.git lib/whisper.cpp -b v1.4.0 --depth=1")
+
+  info("Patch whisper.cpp")
+  fs.cpSync(
+    path.resolve(__dirname, "./whisper-addon.cpp"), 
+    path.resolve(__dirname, "../lib/whisper.cpp/examples/addon.node/addon.cpp"),
+  )
+}
+
+if(!fs.existsSync(path.resolve(__dirname, "../lib/whisper.cpp/build/Release/whisper-addon.node"))) {
+  info("Build whisper.cpp addon")
   shelljs.exec("cd ./lib/whisper.cpp && npx cmake-js compile -T whisper-addon -B Release") 
 }
 
 if(
   (
-    fs.existsSync("./lib/whisper.cpp/build/Release/whisper-addon.node") 
-    || fs.existsSync("./lib/whisper.cpp/build/bin/Release/whisper-addon.node") 
+    fs.existsSync(path.resolve(__dirname, "../lib/whisper.cpp/build/Release/whisper-addon.node")) 
+    || fs.existsSync(path.resolve(__dirname, "./lib/whisper.cpp/build/bin/Release/whisper-addon.node")) 
   )
-  && !fs.existsSync("./public/resources/whisper-addon.node")
+  && !fs.existsSync(path.resolve(__dirname, "../public/resources/whisper-addon.node"))
 ) {
+  info("Copy whisper.cpp addon")
   if(process.platform === "win32") {
-    fs.cpSync("./lib/whisper.cpp/build/bin/Release/whisper-addon.node", "./public/resources/whisper-addon.node")
+    fs.cpSync(
+      path.resolve(__dirname, "../lib/whisper.cpp/build/bin/Release/whisper-addon.node"), 
+      path.resolve(__dirname, "../public/resources/whisper-addon.node"),
+    )
   } else {
-    fs.cpSync("./lib/whisper.cpp/build/Release/whisper-addon.node", "./public/resources/whisper-addon.node")
+    fs.cpSync(
+      path.resolve(__dirname, "../lib/whisper.cpp/build/Release/whisper-addon.node"), 
+      path.resolve(__dirname, "../public/resources/whisper-addon.node"),
+    )
   }
 }
 
-if(!fs.existsSync("./lib/vad/CMakeLists.txt")) {
+if(!fs.existsSync(path.resolve(__dirname, "../lib/vad/CMakeLists.txt"))) {
+  info("Clone VAD-addon")
   shelljs.exec("git clone https://github.com/chenqianhe/VAD-addon lib/vad --depth=1")
 }
 
-if(!fs.existsSync("./lib/vad/build/Release/vad_addon.node")) {
+if(!fs.existsSync(path.resolve(__dirname, "../lib/vad/build/Release/vad_addon.node"))) {
+  info("Build VAD-addon")
   shelljs.exec("cd lib/vad && npm install && npx gulp")
-  fs.cpSync("./lib/vad/silero_vad.onnx", "./public/resources/silero_vad.onnx")
+  info("Copy VAD-addon model")
+  fs.cpSync(
+    path.resolve(__dirname, "../lib/vad/silero_vad.onnx"), 
+    path.resolve(__dirname, "../public/resources/silero_vad.onnx"),
+  )
 }
 
 if(
-  fs.existsSync("./lib/vad/build/Release/vad_addon.node")
-  && !fs.existsSync("./public/resources/vad_addon.node")
+  fs.existsSync(path.resolve(__dirname, "../lib/vad/build/Release/vad_addon.node"))
+  && !fs.existsSync(path.resolve(__dirname, "../public/resources/vad_addon.node"))
 ) {
-  fs.cpSync("./lib/vad/build/Release/vad_addon.node", "./public/resources/vad_addon.node")
+  info("Copy VAD-addon")
+  fs.cpSync(
+    path.resolve(__dirname, "../lib/vad/build/Release/vad_addon.node"), 
+    path.resolve(__dirname, "../public/resources/vad_addon.node"),
+  )
 }
--- src/main/whisper/index.ts
+++ src/main/whisper/index.ts
@@ -2,7 +2,7 @@ import path from "path";
 import { promisify } from "node:util";
 
 const prod = import.meta.env.PROD
-const resourcesPath = prod ? process.resourcesPath : path.join(__dirname, "../../../public/resources")
+const resourcesPath = prod ? process.resourcesPath : path.resolve(__dirname, "../../../public/resources")
 
 const { whisper } = require(path.join(
   resourcesPath,
