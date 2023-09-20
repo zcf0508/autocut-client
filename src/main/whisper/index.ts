c247bacceac60374df59012c2ad125c4d7739637
zcf0508 <zcf0508@live.com>
2023-09-20 16:39:01 +0800

    feat: add whisper addon path check

--- src/main/whisper/index.ts
+++ src/main/whisper/index.ts
@@ -1,13 +1,18 @@
+import fs from "fs";
 import path from "path";
 import { promisify } from "node:util";
 
 const prod = import.meta.env.PROD
 const resourcesPath = prod ? process.resourcesPath : path.resolve(__dirname, "../../../public/resources")
 
-const { whisper } = require(path.resolve(
-  resourcesPath,
-  "whisper-addon.node",
-));
+const addonPath = path.resolve(resourcesPath, "./whisper-addon.node")
+
+if(!fs.existsSync(addonPath)) {
+  console.log({addonPath})
+  throw new Error("Whisper addon not found")
+}
+
+const { whisper } = require(addonPath);
 
 type WhisperAsync = (options: {
   language: string, 
